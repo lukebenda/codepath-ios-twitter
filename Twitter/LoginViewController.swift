660d1545edbdb7647add108cc4aef812b6c0d620
Lucas Benda <54371703+lukebenda@users.noreply.github.com>
2022-09-22 17:58:29 -0500

    Added user login feature to button

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -3,22 +3,25 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    
+    @IBOutlet weak var loginButtonRounded: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginButtonRounded.layer.cornerRadius = 20
     }
+    
     
     @IBAction func onLoginButton(_ sender: Any) {
         // Use Post OAuth/request_token to login
         let myUrl = "https://api.twitter.com/oauth/request_token"
-                TwitterAPICaller.client?.login(url: myUrl, success: {
-                    UserDefaults.standard.set(true, forKey: "userLoggedIn")
-                    self.performSegue(withIdentifier: "loginToHome", sender: self)
-                }, failure: { (Error) in
-                    print("Could not log in!")
-                })
-            }
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            //UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
+    }
     
     /*
      // MARK: - Navigation
--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "Ta4jyfCwxJFJnXfokj69DmItB", consumerSecret: "QxvLtZPnb2GtPTgDw1aP5BhpZgG8YYKPHjTSE47ZZgrcGNTVF1")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
