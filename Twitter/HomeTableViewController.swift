6edd10b377121c2e85d137abd574bd09317111b2
Lucas Benda <54371703+lukebenda@users.noreply.github.com>
2022-09-24 13:39:47 -0500

    Created logout function

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,35 @@
+// Twitter
+// HomeTableViewController.swift
+// Created by Lucas Benda on  9/24/22 AT 1:27 PM.
+// Copyright © 2022 Dan. All rights reserved.
+// Using Swift 5.0
+// Running on macOS 12.5
+
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
